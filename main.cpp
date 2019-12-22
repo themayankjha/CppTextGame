926e7739bd8987f458b1923e673552ca8ed03154
Mayank Jha <jhamayank159@gmail.com>
2019-12-22 07:11:54 +0530

    split into different files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "splash.cpp"
+#include "starting.cpp"
+
+using namespace std;
+extern void splash();
+extern void starting();
+int main()
+{
+splash();
+starting();
+	
+}

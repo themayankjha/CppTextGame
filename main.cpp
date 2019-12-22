6e3f64f57810b13d1bc58e8d1ee2f7cdb27f6224
Mayank Jha <jhamayank159@gmail.com>
2019-12-22 11:07:59 +0530

    Revert "Added munu"
    This reverts commit 054d1fb9c21761095236772b0bc44538cde5efe7.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include "splash.cpp"
 #include "starting.cpp"
-#include "menu.cpp"
 
 using namespace std;
 extern void splash();
-extern void menu();
-
+extern void starting();
 int main()
 {
 splash();
+starting();
 	
 }

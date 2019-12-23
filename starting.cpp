ab42de0b31f4de90ade8ee51aa9529e92b37863a
Mayank Jha <jhamayank159@gmail.com>
2019-12-23 12:29:20 +0530

    Added Player class

--- starting.cpp
+++ starting.cpp
@@ -1,7 +1,6 @@
 #include"variables.cpp"
 using namespace std;
-extern int fatigue;
-extern int intelligence;
+extern playerclass player;
 void starting()
 {
     int choice1;
@@ -14,23 +13,33 @@ void starting()
 	if(choice1==1)
 	{
 		cout<<"You Gently Open Your Eyes. You cannot see properly,the light is hurting your eyes.You flinch and close your eyes rapidly."<<endl;
-		fatigue++;
+		cin.ignore();
+		player.fatiguechange(+1);
 		cout <<"Careful, Careful Now. You should stay still, the wound is still open. The bullet is out but try not to move too much and you'll be fine"<< endl;
+		cin.ignore();
 		cout<<"Open your eyes,Slowly,I need to check your pupils."<<endl;
+		cin.ignore();
 	}
 	if(choice1==2)
 	{
-		cout<<"You stay still and do not move, you try to figure out where you are and what happened to you."<<endl;
-		intelligence++;
+		cout<<"You stay still and do not move, you try to figure out where you are and what happened to you."<<endl;\
+		cin.ignore();
+		player.intelligencechange(+1);
 		cout<<"I know you\'re awake soldier, slowly open your eyes , I need to check your pupils."<<endl;
+		cin.ignore();
 		cout<<"You should stay still, the wound is still open. The bullet is out but try not to move too much and you'll be fine"<<endl;
 		cin.ignore();
 	}
 	
 	cout<<" "<<endl;;
 	cout << "You slowly force your eyes open. There is fog all around you. You cannot see anyone, not even the voice that is telling you to open your eyes" << endl;
+	cin.ignore();
 	cout << "Suddenly you feel a sharp light hitting your eyes, you reflexively close your eyes"<<endl;
+	cin.ignore();
 	cout << "Ouch!, You hear yourself cry out"<<endl;
+	cin.ignore();
 	cout << "Stay Still. You hear the soft voice tell you sternly."<<endl;
+	cin.ignore();
 	cout << "You've suffered a mild concussion, It'll be alright, you need some rest and youll be back to killing those germans soon"<<endl;
+	cin.ignore();
 }
--- variables.cpp
+++ variables.cpp
@@ -1,3 +1,20 @@
-int charm=0;
-int intelligence=0;
-int fatigue=0;
+class playerclass
+{
+int charm;
+int intelligence;
+int fatigue;
+public:
+playerclass()
+{
+    charm=0;
+    intelligence=0;
+    fatigue=0;
+}
+void fatiguechange(int a){
+    fatigue=fatigue+a;
+}
+void intelligencechange(int a){
+    intelligence=intelligence+a;
+}
+};
+playerclass player;
